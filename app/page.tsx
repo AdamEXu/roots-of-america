e66681eb78e28811d23ecd94cae16c8794b59997
Adam Xu <adam.yi.xu@gmail.com>
2026-09-10 17:54:49 +0000

    add text :o

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-red-500 bg-[url('/flag_of_us_saintly.jpeg')] bg-cover bg-center bg-no-repeat">
-
+    <div className="w-screen h-screen bg-[url('/flag_of_us_saintly.jpeg')] bg-cover bg-center bg-no-repeat">
+      <div id="hero" className="text-8xl font-bold text-white text-center w-full h-full text-shadow-lg flex justify-center align-center">AMERICA</div>
     </div>
   );
 }
- 
